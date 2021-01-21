bfcf0a6d1887fbcca892cb5481c1b56c17cef401
Oleksii Makieiev <oleksii.makieiev@teamdev.com>
2021-01-21 21:06:22 +0200

    Implement RegistrationServlet

--- JavaWeb/src/main/java/ua/karazin/ilyin/javaweb/web/listener/ContextListener.java
+++ JavaWeb/src/main/java/ua/karazin/ilyin/javaweb/web/listener/ContextListener.java
@@ -1,5 +1,8 @@
 package ua.karazin.ilyin.javaweb.web.listener;
 
+import ua.karazin.ilyin.javaweb.dao.DBUtils;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -8,7 +11,8 @@ import javax.servlet.annotation.WebListener;
 public class ContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
-
+        ServletContext servletContext = servletContextEvent.getServletContext();
+        servletContext.setAttribute("db_utils", new DBUtils());
     }
 
     @Override
--- JavaWeb/src/main/java/ua/karazin/ilyin/javaweb/web/servlet/RegistrationServlet.java
+++ JavaWeb/src/main/java/ua/karazin/ilyin/javaweb/web/servlet/RegistrationServlet.java
@@ -1,8 +1,42 @@
 package ua.karazin.ilyin.javaweb.web.servlet;
 
+import ua.karazin.ilyin.javaweb.dao.DBUtils;
+import ua.karazin.ilyin.javaweb.entity.Role;
+import ua.karazin.ilyin.javaweb.entity.User;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebListener("/register")
 public class RegistrationServlet extends HttpServlet {
+
+    private DBUtils dbUtils;
+
+    @Override
+    public void init() throws ServletException {
+        ServletContext servletContext = getServletContext();
+        dbUtils = (DBUtils) servletContext.getAttribute("db_utils");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/registration.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+
+        User user = new User();
+        user.setLogin(req.getParameter("username"));
+        user.setPassword(req.getParameter("password"));
+        user.setRole(new Role(2, "user"));
+    }
+
 }
